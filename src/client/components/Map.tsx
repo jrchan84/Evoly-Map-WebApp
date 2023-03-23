836213560907529e0d671a35bd27b2fc5e078e66
jrchan84 <justinrichardchan@gmail.com>
2023-03-23 01:02:27 -0700

    basic Mapbox map working

--- src/client/components/Map.tsx
+++ src/client/components/Map.tsx
@@ -1,11 +1,30 @@
 import React, { useRef, useEffect, useState } from 'react';
 import '../scss/map.scss';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianJjaGFuIiwiYSI6ImNsZmpscmkyMDAyM2o0NWx6NTRrNTIyMnEifQ.j0wbrzIDQhtK9eeWPaLU3Q';
 
 function Map() {
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-70.9);
+	const [lat, setLat] = useState(42.35);
+	const [zoom, setZoom] = useState(9);
+
+	useEffect(() => {
+		if (map.current) return; // initialize map only once
+			map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/streets-v12',
+			center: [lng, lat],
+			zoom: zoom
+		});
+	});
 
     return (
 		<div className="map">
-			<h1>Map</h1>
+			<div ref={mapContainer} className="map-container" />
 		</div>
 	);
 }
