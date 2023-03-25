b5dd0a1fabbcb226a7370e61139bb8f60044a8de
jrchan84 <justinrichardchan@gmail.com>
2023-03-25 00:12:25 -0700

    log NODE_ENV

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,4 +7,5 @@ app.use(express.static('public'));
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+const env = process.env.NODE_ENV || 'development';
+app.listen(port, () => console.log(`Server listening on port: ${port}, env: ${env}`));
